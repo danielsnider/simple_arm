9ca6bc297849894e54e38f2919efc8353d84deca
danielsnider <danielsnider12@gmail.com>
2017-08-15 21:46:56 -0400

    update

--- src/simple_arm/simple_arm_teleop.py
+++ src/simple_arm/simple_arm_teleop.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python
-import time
 import rospy
 import serial
 import struct
 import numpy as np
 
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float32
 
 class JoyArmSerial:
-    PI = 3.14159265359
-
-    def __init__(self,r = 0,i = 0):
-        self.serialDev = serial.Serial()
-        self.serialDev.port = "/dev/ttyACM0" # SET SERIAL DEVICE
-        self.serialDev.open()
-
+    def __init__(self):
         self.velocities = {
             'wrist_roll': 0,
             'wrist_pitch': 0,
             'small_arm': 0,
             'big_arm': 0,
             'base_yaw': 0,
-            'grip': 0,
-            'winch': 0,
-            'zed_camera': 1500
+            'grip': 0
         }
         self.positions = {
-            'arm_camera': 0,
-            'zed_camera': 0
+            'arm_camera': 0
         }
+        baudrate = rospy.get_param('~baudrate', 9600)
+        self.serialDev = serial.Serial(baudrate=baudrate)
+        self.serialDev.port = rospy.get_param("~serial_dev")
+        self.serialDev.open()
+        self.arm_sub = rospy.Subscriber("/joy_arm", Joy, controller.arm_joy_callback)
 
     def write_serial(self):
-        # MOTOR_VEL_OFFSET = -0.1 # use this when the motors don't respond to this value
-        # for key in ['wrist_roll', 'wrist_pitch', 'small_arm', 'big_arm', 'base_yaw']:
-        #     self.velocities[key] = self.velocities[key] + MOTOR_VEL_OFFSET
-
+      # Execute arm position
         rospy.loginfo('velocities:%s\n' % self.velocities)
         rospy.loginfo('positions:%s\n' % self.positions)
-
-      # Execute arm position
         encoded_position = struct.pack("<fffffffff",
                                         self.velocities['wrist_roll'],
                                         self.velocities['wrist_pitch'],
@@ -47,36 +36,9 @@ class JoyArmSerial:
                                         self.velocities['big_arm'],
                                         self.velocities['base_yaw'],
                                         self.velocities['grip'],
-                                        self.velocities['winch'],
-                                        self.velocities['zed_camera'],
-                                        # self.positions['zed_camera'],
                                         self.positions['arm_camera']
                                         )
         self.serialDev.write(encoded_position)
-
-    def zed_servo_callback(self, data):
-        # Zed servo panning control
-        # pan leftward (left bumper)
-        # pan rightward (right bumper)
-
-        self.velocities['zed_camera'] = 1500
-        if data.buttons[5]: # pan leftward (left bumper)
-            self.velocities['zed_camera'] += 1500
-        if data.buttons[4]: # pan rightward (right bumper)
-            self.velocities['zed_camera'] -= 1500
-
-        # MOVE
-        self.write_serial()
-
-    def zed_servo_180_callback(self, data):
-        # Zed servo panning control
-        # pan leftward (left bumper)
-        # pan rightward (right bumper)
-        self.positions['zed_camera'] = data.data # this value is angular postion in degrees
-
-        # MOVE
-        self.write_serial()
-
 
     def arm_joy_callback(self, data):
         self.velocities['wrist_roll'] = 0
@@ -85,7 +47,6 @@ class JoyArmSerial:
         self.velocities['big_arm'] = 0
         self.velocities['base_yaw'] = 0
         self.velocities['grip'] = 0
-        self.velocities['winch'] = 0
 
         # button 12 = double speed
         SPEED_MULTIPLIER = 1
@@ -132,22 +93,12 @@ class JoyArmSerial:
         if data.buttons[1]:
             self.velocities['grip'] = -1
 
-        # button 7 & 8 = winch
-        if data.buttons[6]:
-            self.velocities['winch'] = 1
-        if data.buttons[7]:
-            self.velocities['winch'] = -1
-        if data.buttons[6] and data.buttons[7]:
-            self.velocities['winch'] = 1337 # special flag to free-wheel
-
         # MOVE ARM
         self.write_serial()
 
 
-controller = JoyArmSerial()
-rospy.init_node("joystick_teleoperation_arm")
-arm_sub = rospy.Subscriber("/joy_arm", Joy, controller.arm_joy_callback)
-zed_servo_sub = rospy.Subscriber("/joy", Joy, controller.zed_servo_callback) # continuous
-# zed_servo_sub = rospy.Subscriber("/zed_servo", Float32, controller.zed_servo_180_callback) # 180
 
-rospy.spin()
+def main():
+    rospy.init_node("joystick_teleoperation_arm")
+    controller = JoyArmSerial()
+    rospy.spin()
